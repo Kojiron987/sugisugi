350b1b00851636c1289e0aa2719c5a69ff7eb9c8
Kojiron987 <kojikojih987@gmail.com>
2018-11-12 16:19:54 +0900

    pgmクラスの追記

---
+++ src/Pgm.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include<string>
+#include<fstream>
+#include<iostream>
+
+class Pgm
+{
+public:
+	Pgm(std::string inputFile);
+	~Pgm();
+
+	int getWidth(void);
+	int getHeight(void);
+	int getColorSize(void);
+	void printPgmToken(void);
+
+
+private:
+
+	void skipComment(const std::ifstream inFile);
+	void setPgm(void);
+	
+	std::string inputFile;
+	std::string magic;
+	int width;
+	int height;
+	int colorSize;
+	int *colorArray;
+
+};
