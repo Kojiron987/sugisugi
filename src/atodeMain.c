39c597c478a07d5e2e583b7e99b3b997874b70e3
Kojiron987 <kojikojih987@gmail.com>
2018-11-13 21:17:57 +0900

    getpgmにfseekを適用する

--- src/atodeMain.c
+++ src/atodeMain.c
@@ -15,6 +15,7 @@ int main(int argc, char* argv[])
   PGM_T pgm;
   char c;
   int color;
+  int filter_flag;
   int *orgColor, *edgedColor;
 
   const char* outFile = "../resources/edgefile.pgm";
@@ -34,25 +35,33 @@ int main(int argc, char* argv[])
       edgedColor[point] = orgColor[point];
     }
 
-  /*
-  printf("フィルタの選択\n 1: \n2: \n3: 4:\n");
+  // 使うフィルタの選択
 
-  switch () {
-    case:
+  printf("フィルタの選択\n1: 平滑フィルタ9\n2: 平滑フィルタ16\n3: Golayフィルタ\n4: 平均化フィルタ\n> ");
+  scanf("%d", &filter_flag);
+  switch (filter_flag) {
+  case 1:
+    weighted_avarage(pgm.width, pgm.height, orgColor, edgedColor);
+    break;
+  case 2:
+    weighted_avarage_ver2(pgm.width, pgm.height, orgColor, edgedColor);
+    break;
+  case 3:
+    golay_filter(pgm.colorSize, pgm.width, pgm.height, orgColor, edgedColor);
+    break;
+  case 4:
+     median_filter(pgm.width, pgm.height, orgColor, edgedColor);
+    break;
+  default:
+    printf("不正な入力です\n");
+    exit(1);
+    break;
   }
 
-  */
-
-  weighted_avarage(pgm.width, pgm.height, orgColor, edgedColor);
-//  weighted_avarage_ver2(pgm.width, pgm.height, orgColor, edgedColor);
-//  golay_filter(pgm.colorSize, pgm.width, pgm.height, orgColor, edgedColor);
-//  median_filter(pgm.width, pgm.height, orgColor, pgm.edgedColor);
-
-
-
-
+  // フィルタを通した画像データの出力
   putEdgedPgm(outFile, edgedColor, &pgm);
 
+  // メモリの開放
   free(orgColor);
   free(edgedColor);
   return 0;
